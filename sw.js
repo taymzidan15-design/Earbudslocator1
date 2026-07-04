b6666358ed9a1603d5dbe8da4b4c6b77dea6a402
taymzidan15-design <taym.zidan15@gmail.com>
2026-07-04 23:09:47 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "findbud-v1";
+const FILES = ["./find-my-earbud.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
